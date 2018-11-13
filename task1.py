b33eb58138a2f28b412236fe55aa3632e57bc5c8
nikitaglytov <44898741+nikitaglytov@users.noreply.github.com>
2018-11-13 15:42:14 +0300

    correct1
    correct1

--- task1.py
+++ task1.py
@@ -1,12 +1,12 @@
 N = input("Please enter the number of students: ")
 list1 = []
 for i in range(int(N)):
-    Name = input("Name of {number} student: ".format(number=i + 1))
+    n = input("Name of {number} student: ".format(number=i + 1))
     M = input("Math marks: ".format(number=i + 1))
     Ph = input("Physics marks: ".format(number=i + 1))
     Ch = input("Chemistry marks: ".format(number=i + 1))
-    list1.append({'Name': Name, 'M': float(M), 'Ph': float(Ph), 'Ch': float(Ch)})
+    list1.append({'n': n, 'M': float(M), 'Ph': float(Ph), 'Ch': float(Ch)})
 find_name = input("Find name of student:")
 for i in range(int(N)):
-    if (list1[i]['Name'] == find_name):
+    if (list1[i]['n'] == find_name):
         print((list1[i]['M'] + list1[i]['Ph'] + list1[i]['Ch']) / 3)
