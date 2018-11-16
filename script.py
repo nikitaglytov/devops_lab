1b871718e7f1315e2f4426a943f6871d0aecf672
nikitaglytov <44898741+nikitaglytov@users.noreply.github.com>
2018-11-16 16:31:14 +0300

    hw3

--- script.py
+++ script.py
@@ -7,16 +7,16 @@ import psutil
 from time import sleep
 
 
-
-
 def status():
     get_info = {"CPU_load": str(psutil.cpu_percent()) + "%",
                 "memory_size": str(psutil.Process().memory_info().rss // (1024 * 1024)) + " MB",
                 "memory_usage": str(psutil.virtual_memory().used // (1024 * 1024)) + " MB",
-                "IO": str(psutil.disk_io_counters().write_bytes / (1024 * 1024)),             
+                "IO": str(psutil.disk_io_counters().write_bytes / (1024 * 1024)),
                 "Network": str(psutil.net_io_counters().bytes_sent / (1024 * 1024))
                 }
     return get_info
+
+
 counter = 0
 if output == "text":
     while True:
@@ -42,4 +42,3 @@ if output == "json":
         output_file.close()
         get_status.clear()
         counter += 1
-        sleep(interval)
