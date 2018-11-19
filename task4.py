18b7366063ca6d01aa83aead046e5629710fc48e
nikitaglytov <44898741+nikitaglytov@users.noreply.github.com>
2018-11-19 09:07:17 +0300

    Delete task4.py

--- task4.py
+++
@@ -1,9 +0,0 @@
-N, M = map(int, input().split())
-a = "-"
-b = ".|."
-c = "WELCOME"
-for i in range(1, N, 2):
-    print((a * ((M - (i * 3)) // 2)) + (b * i) + (a * ((M - (i * 3)) // 2)))
-print("-" * ((M - 7) // 2) + c + "-" * ((M - 7) // 2))
-for i in range(N - 2, - 1, - 2):
-    print((a * ((M - (i * 3)) // 2)) + (b * i) + (a * ((M - (i * 3)) // 2)))
